e36cf85b879424e788e2ad76d913a0119c4500da
bebroch <97059293+bebroch@users.noreply.github.com>
2022-01-09 18:22:36 +0300

    Добавьте файлы проекта.

---
+++ калькулятор на проект/UserControl1.Designer.cs
@@ -0,0 +1,82 @@
+﻿
+namespace калькулятор_на_проект
+{
+    partial class journalOperation
+    {
+        /// <summary> 
+        /// Обязательная переменная конструктора.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary> 
+        /// Освободить все используемые ресурсы.
+        /// </summary>
+        /// <param name="disposing">истинно, если управляемый ресурс должен быть удален; иначе ложно.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Код, автоматически созданный конструктором компонентов
+
+        /// <summary> 
+        /// Требуемый метод для поддержки конструктора — не изменяйте 
+        /// содержимое этого метода с помощью редактора кода.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.label1 = new System.Windows.Forms.Label();
+            this.label2 = new System.Windows.Forms.Label();
+            this.SuspendLayout();
+            // 
+            // label1
+            // 
+            this.label1.Anchor = ((System.Windows.Forms.AnchorStyles)((System.Windows.Forms.AnchorStyles.Top | System.Windows.Forms.AnchorStyles.Right)));
+            this.label1.AutoSize = true;
+            this.label1.Font = new System.Drawing.Font("Microsoft Sans Serif", 14F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(204)));
+            this.label1.ForeColor = System.Drawing.SystemColors.ControlDark;
+            this.label1.Location = new System.Drawing.Point(191, 12);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(60, 24);
+            this.label1.TabIndex = 0;
+            this.label1.Text = "label1";
+            // 
+            // label2
+            // 
+            this.label2.Anchor = ((System.Windows.Forms.AnchorStyles)((System.Windows.Forms.AnchorStyles.Top | System.Windows.Forms.AnchorStyles.Right)));
+            this.label2.AutoSize = true;
+            this.label2.Font = new System.Drawing.Font("Microsoft Sans Serif", 18F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(204)));
+            this.label2.Location = new System.Drawing.Point(172, 36);
+            this.label2.Name = "label2";
+            this.label2.Size = new System.Drawing.Size(79, 29);
+            this.label2.TabIndex = 1;
+            this.label2.Text = "label2";
+            // 
+            // journalOperation
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.BackColor = System.Drawing.Color.White;
+            this.Controls.Add(this.label2);
+            this.Controls.Add(this.label1);
+            this.Name = "journalOperation";
+            this.Size = new System.Drawing.Size(264, 82);
+            this.MouseDown += new System.Windows.Forms.MouseEventHandler(this.journalOperation_MouseDown);
+            this.MouseEnter += new System.EventHandler(this.journalOperation_MouseEnter);
+            this.MouseLeave += new System.EventHandler(this.journalOperation_MouseLeave);
+            this.MouseUp += new System.Windows.Forms.MouseEventHandler(this.journalOperation_MouseUp);
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.Label label2;
+    }
+}
---
+++ калькулятор на проект/UserControl1.cs
@@ -0,0 +1,28 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace калькулятор_на_проект
+{
+    public partial class journalOperation : UserControl
+    {
+        public journalOperation()
+        {
+            InitializeComponent();
+        }
+
+        private void journalOperation_MouseEnter(object sender, EventArgs e) => BackColor = Color.LightGray;
+
+        private void journalOperation_MouseLeave(object sender, EventArgs e) => BackColor = Color.White;
+
+        private void journalOperation_MouseDown(object sender, MouseEventArgs e) => BackColor = Color.Gray;
+
+        private void journalOperation_MouseUp(object sender, MouseEventArgs e) => BackColor = Color.White;
+    }
+}
